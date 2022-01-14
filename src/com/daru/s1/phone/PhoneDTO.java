401a1eda44247dbd1d8a4baea3b8c106a434028f
Jeonjueun07 <lovesj2323@naver.com>
2022-01-14 23:21:09 +0900

    Study

---
+++ src/com/daru/s1/phone/PhoneDTO.java
@@ -0,0 +1,45 @@
+package com.daru.s1.phone;
+
+public class PhoneDTO {
+
+	private String name;
+	private String nickname;
+	private String email;
+	private String num;
+	private String birth;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	
+	
+	
+}
