27bbe180a60edf0789658f8a71a84568209da379
Jeonjueun07 <lovesj2323@naver.com>
2022-01-17 19:18:40 +0900

    Study

--- src/com/daru/s1/phone/PhoneNum1.java
+++ src/com/daru/s1/phone/PhoneNum1.java
@@ -1,5 +1,23 @@
 package com.daru.s1.phone;
 
+import java.util.List;
+
 public class PhoneNum1 {
 
+	public void showData(List<PhoneDTO> ar ) {
+		
+		for(int i = 0; i<ar.size();i++) {
+			PhoneDTO phoneDTO = ar.get(i);//선언과 초기화동시에함
+			System.out.println("Name :"+phoneDTO.getName());
+			System.out.println("Nickname :"+phoneDTO.getNickname());
+			System.out.println("Email :"+phoneDTO.getEmail());
+			System.out.println("Phone :"+phoneDTO.getNum());
+			System.out.println("Birth :"+phoneDTO.getBirth());
+
+
+
+		}
+		
+	}
+	
 }
